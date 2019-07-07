08af772112c3e734e2633425471779ad1f1ec56d
DharaniMuli <46985863+DharaniMuli@users.noreply.github.com>
2019-07-07 18:36:42 -0500

    Created procfile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,18 @@ import { HttpClient, HttpHeaders, HttpErrorResponse} from '@angular/common/http'
 })
 export class AppComponent implements OnInit {
   title = 'Our Store App';
-  // uri = 'http://localhost:4000/items/add';
+   // uri = 'http://localhost:4000/items/add';
   uri = 'http://storeappbackend.herokuapp.com/items/add';
-  // geturi = 'http://localhost:4000/items/find'
+   // geturi = 'http://localhost:4000/items/find'
   geturi = 'https://storeappbackend.herokuapp.com/items/find'
+
+  edituri = 'http://localhost:4000/items/edit';
+  // uri = 'http://storeappbackend.herokuapp.com/items/edit';
+
+  deluri = 'http://localhost:4000/items/edit';
+  // uri = 'http://storeappbackend.herokuapp.com/items/edit';
+
+
   item = {
     Description: '',
     Cost: ''
@@ -23,11 +31,7 @@ export class AppComponent implements OnInit {
 
   constructor(private router: Router, private http: HttpClient) {  }
   ngOnInit() {
-    this.http.get(`${this.geturi}`).subscribe(res => {
-      console.log(res);
-      this.itemlist = res;
-      return this.itemlist;
-    });
+    this.getItem();
   }
     addItem = () => {
       this.http.post(`${this.uri}`, this.item).subscribe(data => {
@@ -38,5 +42,22 @@ export class AppComponent implements OnInit {
         };
         console.log('After Backend call', +data);
     });
+  }
+  getItem = () => {
+    this.http.get(`${this.geturi}`).subscribe(res => {
+      console.log(res);
+      this.itemlist = res;
+      return this.itemlist;
+    });
+
+  // editItem = () => {
+  //   this.http.post(`${this.edituri}`, this.item).subscribe(data => {
+  //     this.itemlist.push(this.item);
+  //     this.item = {
+  //       Description: '',
+  //       Cost: ''
+  //     };
+  //     console.log('After Backend call', +data);
+  //   });
   }
 }
